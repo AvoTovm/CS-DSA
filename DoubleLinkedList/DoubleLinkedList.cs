bce711b519f33a7a6b0625659fd77fb6a0111115
Avo <atevttovmas@gmail.com>
2025-03-08 14:36:11 +0400

    Update DoubleLinkList

---
+++ DoubleLinkedList/DoubleLinkedList.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Data;
+
+namespace DoubleLinkedList
+{
+    public class DoubleLinkedList
+    {
+        private Node head;
+        private Node tail;
+
+        public void Traverse()
+        {
+            Node curr = head;
+            while(curr != null)
+            {
+                Console.Write(curr.data + " ");
+                curr = curr.next;
+            }
+            Console.WriteLine("null ");
+        }
+
+        public void AddFirst(int value)
+        {
+            Node newNode = new Node(value);
+            if(head == null)
+            {
+                head = tail = newNode;
+                return;
+            }
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+
+        public void AddLast(int value)
+        {
+            Node newNode = new Node(value);
+            if(tail == null)
+            {
+                tail = head = newNode;
+                return;
+            }
+
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+
+        public void AddAtPos(int value, int pos)
+        {   
+            if(pos < 1) throw new Exception("position is out of range");
+            if(pos == 1) AddFirst(value);
+
+            Node newNode = new Node(value);
+            Node curr = head;
+
+            for(int i = 0; i < pos - 1 && curr != null; i++)
+            {
+                curr = curr.next;
+            }
+
+            if(curr == null) throw new Exception("out of bounds");
+
+            newNode.next = curr.next;
+            newNode.prev = curr;
+
+            if(curr.next != null) curr.next.prev = newNode;
+            curr.next = newNode;
+
+            if(curr == tail) tail = newNode;
+        }
+
+        public void RemoveAtBegin()
+        {
+            if(head == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+            Node temp = head;
+            if(head == tail && head != null)
+            {
+                head = tail = null;
+            }
+            else
+            {
+                head = head.next;
+                head.prev = null;
+            }
+        }
+
+        public void RemoveAtEnd()
+        {
+            if(head == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+
+            Node node = tail;
+            if(tail == head && tail != null)
+            {
+                tail = head = null;
+            }
+            else{
+                tail = tail.prev;
+                tail.next = null;
+            }
+        }
+
+        public void RemoveAtPos(int pos)
+        {
+            if(pos < 1) throw new Exception("position is out of range");
+            if(pos == 1)
+            {
+                RemoveAtBegin();
+                return;
+            }
+
+            Node temp = head;
+            for(int i = 0; i < pos - 1 && temp != null; i++) temp = temp.next;
+
+            if(temp == null) throw new Exception("position is out of bounds");
+
+            if(temp.next != null) temp.next.prev = temp.prev;
+            if(temp.prev != null) temp.prev.next = temp.next;
+
+            if(temp == tail) tail = temp.prev;
+        }
+    }
+}
---
+++ DoubleLinkedList/main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DoubleLinkedList
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            DoubleLinkedList list = new DoubleLinkedList();
+
+            list.AddFirst(1);
+            list.AddFirst(2);
+            list.AddLast(4);
+            list.AddLast(2);
+            list.AddAtPos(8, 2);
+
+            Console.WriteLine("list traverse ");
+            list.Traverse();
+
+            list.RemoveAtBegin();
+            list.RemoveAtEnd();
+            list.RemoveAtPos(2);
+
+            Console.WriteLine("list aftert remove");    // 1
+            list.Traverse();
+        }
+    }
+}
