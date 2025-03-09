91c69c5d155056fc97981c77d93b6c1cf3046c7e
Avo <atevttovmas@gmail.com>
2025-03-09 22:09:28 +0400

    HeapTree commit

---
+++ HeapTree/HeapTree.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HeapTree
+{
+    public class HeapTree
+    {
+        private List<int> heap;
+
+        public HeapTree()
+        {
+            heap = new List<int>();
+        }
+
+        public void PrintHeap()
+        {
+            for(int i = 0; i < heap.Count; i++)
+            {
+                Console.Write(heap[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void Insert(int key)
+        {
+            heap.Add(key);
+            int index = heap.Count - 1;
+
+            while(index > 0 && heap[(index - 1) / 2] < heap[index])
+            {
+                (heap[(index - 1) / 2], heap[index]) = (heap[index], heap[(index - 1) / 2]);
+                index = (index - 1) / 2;
+            }
+
+        }
+
+        public void ExtractValue(int key)
+        {
+            int size = heap.Count;
+            if (size == 0) throw new Exception("heap is empty");
+
+            int index = -1;
+            for(int i = 0; i < size; i++)
+            {
+                if(heap[i] == key)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index == -1) throw new Exception("Value not found");
+            heap[index] = heap.Last();
+            heap.RemoveAt(heap.Count - 1);
+
+            int largest = index;
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            size = heap.Count;
+
+            while (true)
+            {
+                if(left < size && heap[left] > heap[largest])
+                {
+                    largest = left;
+                }if(right < size && heap[right] > heap[largest])
+                {
+                    largest = right;
+                }
+
+                if(largest != index)
+                {
+                    (heap[index], heap[largest]) = (heap[largest], heap[index]);
+                    index = largest;
+                    left = 2 * index + 1;
+                    right = 2 * index + 2;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ HeapTree/main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace HeapTree
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            HeapTree heap = new HeapTree();
+
+            heap.Insert(10);
+            heap.Insert(20);
+            heap.Insert(5);
+            heap.Insert(30);
+
+            heap.PrintHeap();
+
+            heap.ExtractValue(30);
+            heap.PrintHeap();
+        }
+    }
+}
